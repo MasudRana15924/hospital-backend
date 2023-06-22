a2650c7f94c9e011bcff7d285654b094bc63021b
MasudRana15924 <masudrana15924@outlook.com>
2023-06-23 01:59:21 +0600

    doctor model added

--- Controllers/doctors.js
+++ Controllers/doctors.js
@@ -1,74 +1,81 @@
-const newDoctorModel = require('../models/Doctor');
-const sendToken = require('../utilies/jwtToken');
-const SendEmail = require('../utilies/sendEmail');
-const jwt = require("jsonwebtoken");
-const cloudinary = require('cloudinary');
-const crypto = require("crypto");
-const ErrorHandler = require("../utilies/ErrorHandler");
-const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+// const doctorModel = require('../models/Doctors');
+// const sendToken = require('../utilies/jwtToken');
+// const SendEmail = require('../utilies/sendEmail');
+// const jwt = require("jsonwebtoken");
+// const cloudinary = require('cloudinary');
+// const crypto = require("crypto");
+// const ErrorHandler = require("../utilies/ErrorHandler");
+// const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 
 
-exports.registerDoctor =catchAsyncErrors( async (req, res, next) => {
-    try {
-        const {title, name,gender,birthdate,district,nid_No,bmdc_No,type,phone, email, password } = req.body;
-        const findDoctor = await newDoctorModel.findOne({ email: email });
-        if (findDoctor) {
-            return next(new ErrorHandler("doctor already exists", 400));
-        }
-        const doctor = await newDoctorModel.create({
-            title, name,gender,birthdate,district,nid_No,bmdc_No,type,phone, email, password
-          });
-        
-          sendToken(doctor, 201, res);
-          SendEmail({
-                 email: doctor.email,
-                subject: "Activate Your Account",
-             message: `Hello ${doctor.name}, your account is create`,
-    
-             });
+// exports.registerDoctor = catchAsyncErrors(async (req, res, next) => {
+//     try {
+//         const { title, name, gender, birthdate, district, nid_No, bmdc_No, type, phone, email, password, work, expert, degree,experience } = req.body;
+//         // const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+//         //     folder: "avatars",
+//         //     width: 150,
+//         //     crop: "scale",
+//         // });
+//         const findDoctor = await doctorModel.findOne({ email: email });
+//         if (findDoctor) {
+//             return next(new ErrorHandler("doctor already exists", 400));
+//         }
+//         const doctor = await doctorModel.create({
+//             title, name, gender, birthdate, district, nid_No, bmdc_No, type,
+//             phone, email, password, work, expert, degree,experience
+//             // avatar: {
+//             //     public_id: myCloud.public_id,
+//             //     url: myCloud.secure_url,
+//             // },
+//         });
 
-        // const user = {
-        //     name: name,
-        //     email: email,
-        //     password: password,
+//         sendToken(doctor, 201, res);
+//         SendEmail({
+//             email: doctor.email,
+//             subject: "Activate Your Account",
+//             message: `Hello ${doctor.name}, your account is create`,
+//         });
+//     } catch (error) {
+//         return next(new ErrorHandler(error.message, 400));
+//     }
+// });
+// exports.loginDoctor = async (req, res, next) => {
+//     const { email, password } = req.body;
+//     if (!email || !password) {
+//         res.json({ message: "Please Enter Email & Password" });
+//     }
+//     const doctor = await doctorModel.findOne({ email }).select("+password");
 
-        // };
-        // const activationToken = createActivationToken(user);
-        // const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+//     if (!doctor) {
+//         return next(new ErrorHandler("Doctor doesn't exists!", 400));
+//     }
+//     const isPasswordMatched = await doctor.comparePassword(password);
+//     if (!isPasswordMatched) {
+//         return next(
+//             new ErrorHandler("Email & password does not matched", 400)
+//         );
+//     }
+//     if (isPasswordMatched) {
 
-        // res.status(201).json({
-        //     success: true,
-        //     message: `please check your email:- ${user.email} to activate your account!`,
-        // });
-        //  SendEmail({
-        //     email: user.email,
-        //     subject: "Activate your account",
-        //     message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+//         sendToken(doctor, 200, res);
+//         await newDoctorModel.updateOne({ email }, { $set: { isActive: 'true' } })
+//         // res.send(updateDoctor);
 
-        // });
-    } catch (error) {
-        return next(new ErrorHandler(error.message, 400));
-    }
-});
-exports.loginDoctor = async (req, res, next) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        res.json({ message: "Please Enter Email & Password" });
-    }
-    const doctor = await newDoctorModel.findOne({ email }).select("+password");
-    if (!doctor) {
-        return next(new ErrorHandler("Doctor doesn't exists!", 400));
-    }
-    const isPasswordMatched = await doctor.comparePassword(password);
-    if (!isPasswordMatched) {
-        return next(
-            new ErrorHandler("Email & password does not matched", 400)
-        );
-    }
-    if (isPasswordMatched) {
-        sendToken(doctor, 200, res);
-    }
-    else {
-        res.json({ message: "Please valid Password" });
-    }
-};
+//     }
+
+//     else {
+//         res.json({ message: "Please valid Password" });
+//     }
+// };
+// exports.logoutDoctor = async (req, res) => {
+//     // res.cookie("token", null, {
+//     //     expires: new Date(Date.now()),
+//     //     httpOnly: true,
+//     // });
+//     const { email } = req.body;
+//     const updateDoctor = await doctorModel.updateOne({ email }, { $set: { isActive: 'false' } })
+//     res.status(200).json({
+//         success: true,
+//         updateDoctor
+//     });
+// };
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,8 @@
 const userModel = require('../models/User');
 const jwt = require("jsonwebtoken");
 exports.isAuthenticatedUser = async (req, res, next) => {
-   const token = req.header("Authorization");
-  // const { token } = req.cookies;
+  //  const token = req.header("Authorization");
+  const { token } = req.cookies;
   if (!token) {
     res.status(401).json({ message: "Please Login to access" });
   }
