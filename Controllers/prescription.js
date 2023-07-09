302b163a20ab8fe68fceee7f54d90353a191a37c
MasudRana15924 <masudrana15924@outlook.com>
2023-07-10 00:24:25 +0600

    prescription added

--- Controllers/prescription.js
+++ Controllers/prescription.js
@@ -4,11 +4,14 @@ const prescriptionModel = require('../models/prescription');
 
 // only admin access this 
 exports.createPrescription = async (req, res) => {
+  console.log(req.body);
     const prescription= await prescriptionModel.create(req.body);
+    console.log(prescription);
     res.status(201).json({
         success: true,
         prescription
       });
+     
 };
 exports.myPrescription = async (req, res, next) => {
     const prescription = await prescriptionModel.find({ userId: req.user._id }).sort({createdAt:-1});
